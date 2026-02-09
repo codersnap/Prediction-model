2703aa27445382af25e6d22d0b0e88f92cc6fbd9
Prathamesh <prathamesh.s0805@gmail.com>
2026-02-09 11:21:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,9 @@ st.info('A machine learning model for prediction')
 
 with st.expander('Data'):
     st.write('**Raw Data**')
-    df=pd.read_csv('https://github.com/codersnap/Prediction-model/blob/master/car_service_dataset_1000_rows.csv')
+    df=pd.read_csv('https://raw.githubusercontent.com/codersnap/Prediction-model/master/car_service_dataset_1000_rows.csv')
     df
 
-    st.write('**x**')
-    x_raw=df.drop('species',axis=1)
-    x_raw
-
-
-    st.write('**y**')
-    y_raw=df.species
-    y_raw
+    
 
 
